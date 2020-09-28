1168154ae31b5cfbf5341bf26a3c7a18c45d56b6
adigowda <adithyagowda69@gmail.com>
2020-09-29 00:52:36 +0530

    Add fields for profile

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const isAuthenticated = require('../../middleware/auth')
-const User = require('../../models/User')
 const authController = require('../../controllers/authController')
 
 router.get(
